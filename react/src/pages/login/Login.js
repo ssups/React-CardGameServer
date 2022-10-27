355c32fdb7587952fd1d49b9090cca3dc9904ae1
ssups <shalstlqruki@gmail.com>
2022-10-28 01:43:39 +0900

    audio

--- react/src/pages/login/Login.js
+++ react/src/pages/login/Login.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Whole, BackGround, Wrap } from "../login/style";
 import { login } from "../../images";
 import { useNavigate } from "react-router-dom";
 import { JoinComp, LoginComp } from "../../components";
+import AudioPlayer from "react-h5-audio-player";
+import { rain } from "../../sounds";
 
 const Login = ({ setIsLoginPage, setOnLoad }) => {
   const nav = useNavigate();
   const [switched, setSwitched] = useState(false);
+  const audioRef = useRef();
   useEffect(() => {
     setIsLoginPage(true);
     // 해당 컴포넌트 언마운트때 실행
@@ -21,6 +24,14 @@ const Login = ({ setIsLoginPage, setOnLoad }) => {
   }
   return (
     <Whole>
+      <AudioPlayer
+        src={rain}
+        autoPlay={true}
+        ref={audioRef}
+        loop={true}
+        volume={0.3}
+        style={{ display: "none" }}
+      />
       <BackGround src={login}></BackGround>
       <Wrap>
         {switched ? (

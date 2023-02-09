a51bf46bc438e0c1476309f8ec232559ae3df1ef
ssups <shalstlqruki@gmail.com>
2023-02-10 03:57:20 +0900

    port

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const path = require('path');
 
 dot.config();
 
+// 프론트 구동중인 포트 접근 허용해주기
+const options = {
+  origin: 'http://3.36.55.166',
+};
+app.use(cors(options));
+
 // 서버열기
 app.listen(PORT, () => {
   console.log(PORT, '번 포트에 서버 열림');
@@ -19,11 +25,6 @@ sequelize
   .then(() => console.log('디비 연결 잘됨'))
   .catch(err => console.log(err));
 
-// 프론트 구동중인 포트 접근 허용해주기
-const options = {
-  origin: 'http://3.36.55.166',
-};
-app.use(cors(options));
 app.use(express.json()); // 객체형태 전달받을때 해석해주는 기능
 
 // 리액트 열기

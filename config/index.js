dcaeab9c3f962bbf273a7f36c095bdaa38495d85
ssups <shalstlqruki@gmail.com>
2023-02-10 03:38:51 +0900

    url change

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 const config = {
   dev: {
-    username: "root",
-    password: "Admin123!",
-    database: "react_card_game",
+    username: 'root',
+    password: 'Admin123!',
+    database: 'react_card_game',
     // host: "127.0.0.1",
-    host: "localhost",
-    dialect: "mysql",
-    timezone: "+09:00",
-    port: "3306",
+    host: 'ec2-3-36-55-166.ap-northeast-2.compute.amazonaws.com',
+    dialect: 'mysql',
+    timezone: '+09:00',
+    port: '3306',
   },
 };
 // dzdzdz

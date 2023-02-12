d86a7244357bb53a8839b3f9989cc0850821cd1b
ssups <shalstlqruki@gmail.com>
2023-02-12 23:37:04 +0900

    port chang

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 80;
 const { sequelize } = require('./model');
 const cors = require('cors');
 const dot = require('dotenv');
@@ -30,30 +30,26 @@ app.use(express.json()); // 객체형태 전달받을때 해석해주는 기능
 // 리액트 열기
 app.use(express.static(path.join(__dirname, 'react/build')));
 app.use(express.static(path.join(__dirname, 'react/public')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-app.get('/main', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-app.get('/shop', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-app.get('/card_book', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-app.get('/public_board', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-app.get('/notice_board', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-app.get('/mypage', (req, res) => {
-  res.sendFile(path.join(__dirname, 'react/build/index.html'));
-});
-// 위에처럼 하나하나 안할라면 밑에 * 이걸로 전역 get 걸어놓고 데이터 요청방식 전부 post로 바꾸면됨
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "react/public/index.html"));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
+// });
+// app.get('/main', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
+// });
+// app.get('/shop', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
+// });
+// app.get('/card_book', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
+// });
+// app.get('/public_board', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
+// });
+// app.get('/notice_board', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
+// });
+// app.get('/mypage', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'react/build/index.html'));
 // });
 
 // 라우터
@@ -66,5 +62,9 @@ app.use(image);
 app.use(post);
 app.use(comment);
 
+// 위에처럼 하나하나 안할라면 밑에 * 이걸로 전역 get 걸어놓고 데이터 요청방식 전부 post로 바꾸면됨
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'react/public/index.html'));
+});
 // 정적폴더 경로설정`
 app.use('/images', express.static(path.join(__dirname, '/images')));

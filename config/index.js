ec6a4a26433189bffbf540aabbf5b6fff6e657da
ssups <shalstlqruki@gmail.com>
2023-02-12 23:45:27 +0900

    env

--- config/index.js
+++ config/index.js
@@ -7,7 +7,6 @@ const config = {
     password: process.env.DB_PW,
     database: 'react_card_game',
     host: process.env.HOST,
-    host: 'ec2-3-36-55-166.ap-northeast-2.compute.amazonaws.com',
     dialect: 'mysql',
     timezone: '+09:00',
     port: '3306',

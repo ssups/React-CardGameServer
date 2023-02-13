ffee93539088c48eabc1bf8da6d15d24ff4d05e6
ssups <shalstlqruki@gmail.com>
2023-02-13 16:56:05 +0900

    topPointUser router

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
-const { User, Inventory } = require("../model");
-const bcrypt = require("bcrypt");
+const router = require('express').Router();
+const { User, Inventory } = require('../model');
+const bcrypt = require('bcrypt');
 
-router.post("/join", async (req, res) => {
+router.post('/join', async (req, res) => {
   const { id, nickName, pw } = await req.body;
   // 어드민은 기본 포인트 십만
-  const point = id === "admin" ? 100000 : 0;
+  const point = id === 'admin' ? 100000 : 0;
   const idOverlap = await User.findOne({
     where: { user_id: id },
   });
@@ -13,11 +13,11 @@ router.post("/join", async (req, res) => {
     where: { nick_name: nickName },
   });
   if (idOverlap) {
-    res.send({ success: false, msg: "아이디 중복" });
+    res.send({ success: false, msg: '아이디 중복' });
     return;
   }
   if (nickOverlap) {
-    res.send({ success: false, msg: "닉네임 중복" });
+    res.send({ success: false, msg: '닉네임 중복' });
     return;
   }
   if (!idOverlap && !nickOverlap) {
@@ -35,13 +35,17 @@ router.post("/join", async (req, res) => {
             point_5000: 1,
           })
         )
-        .then(() => res.send({ success: true, msg: "가입 완료" }));
+        .then(() => res.send({ success: true, msg: '가입 완료' }));
     });
   }
 });
 
-router.get("/get_users_point", async (req, res) => {
-  const data = await User.findAll({ attributes: ["point", "user_id"], order: [["point", "desc"]] });
+router.get('/get_users_point', async (req, res) => {
+  const data = await User.findAll({
+    attributes: ['point', 'user_id'],
+    order: [['point', 'desc']],
+    limit: 5,
+  });
   res.send(data);
 });
 

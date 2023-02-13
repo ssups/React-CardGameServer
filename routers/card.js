a8b74676313da3ad78ab40bfb28b9233ae526255
ssups <shalstlqruki@gmail.com>
2023-02-13 16:50:56 +0900

    cardRouter to CardRouter

--- routers/card.js
+++ routers/card.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
-const { User, Inventory } = require("../model");
-const { Card, RareCard, UltraRareCard } = require("../functions/Card");
+const router = require('express').Router();
+const { User, Inventory } = require('../model');
+const { Card, RareCard, UltraRareCard } = require('../functions/card');
 
-router.post("/get_user_cards", async (req, res) => {
-  console.log("겟유저카드실행");
+router.post('/get_user_cards', async (req, res) => {
+  console.log('겟유저카드실행');
   const { id } = req.body;
   const data = await User.findOne({
     where: { user_id: id },
-    attributes: ["cards"],
+    attributes: ['cards'],
     raw: true,
   });
   //   .then(el => console.log(el));
@@ -15,13 +15,13 @@ router.post("/get_user_cards", async (req, res) => {
   res.send(data?.cards);
 });
 
-router.post("/open_card_pack", async (req, res) => {
-  console.log("카드팩 열기 실행");
+router.post('/open_card_pack', async (req, res) => {
+  console.log('카드팩 열기 실행');
   const { id } = req.body;
   console.log(id);
   const init = await User.findOne({
     where: { user_id: id },
-    attributes: ["cards"],
+    attributes: ['cards'],
     raw: true,
     include: {
       model: Inventory,
@@ -30,8 +30,8 @@ router.post("/open_card_pack", async (req, res) => {
   });
   console.log(init);
   const initCards = init.cards ? JSON.parse(init.cards) : [];
-  const initCardPackBasic = init["Inventories.card_pack_basic"];
-  console.log("처음카드", initCards);
+  const initCardPackBasic = init['Inventories.card_pack_basic'];
+  console.log('처음카드', initCards);
   const newCards = Array(5)
     .fill(0)
     .map(el => new Card());
@@ -45,23 +45,23 @@ router.post("/open_card_pack", async (req, res) => {
     }
   );
   await Inventory.update({ card_pack_basic: initCardPackBasic - 1 }, { where: { user_id: id } });
-  res.send("개봉완료");
+  res.send('개봉완료');
 });
 
-router.post("/open_rare_card_pack", async (req, res) => {
-  console.log("레어 카드팩 열기 실행");
+router.post('/open_rare_card_pack', async (req, res) => {
+  console.log('레어 카드팩 열기 실행');
   const { id } = req.body;
   const init = await User.findOne({
     where: { user_id: id },
-    attributes: ["cards"],
+    attributes: ['cards'],
     raw: true,
     include: {
       model: Inventory,
-      attributes: ["card_pack_rare"],
+      attributes: ['card_pack_rare'],
     },
   });
   const initCards = init.cards ? JSON.parse(init.cards) : [];
-  const initCardPackRare = init["Inventories.card_pack_rare"];
+  const initCardPackRare = init['Inventories.card_pack_rare'];
   //   console.log("처음카드", initCards);
   const newCards = Array(3)
     .fill(0)
@@ -76,23 +76,23 @@ router.post("/open_rare_card_pack", async (req, res) => {
     }
   );
   await Inventory.update({ card_pack_rare: initCardPackRare - 1 }, { where: { user_id: id } });
-  res.send("개봉완료");
+  res.send('개봉완료');
 });
 
-router.post("/open_ultraRare_card_pack", async (req, res) => {
-  console.log("울레 카드팩 열기 실행");
+router.post('/open_ultraRare_card_pack', async (req, res) => {
+  console.log('울레 카드팩 열기 실행');
   const { id } = req.body;
   const init = await User.findOne({
     where: { user_id: id },
-    attributes: ["cards"],
+    attributes: ['cards'],
     raw: true,
     include: {
       model: Inventory,
-      attributes: ["card_pack_ultraRare"],
+      attributes: ['card_pack_ultraRare'],
     },
   });
   const initCards = init.cards ? JSON.parse(init.cards) : [];
-  const initCardPackUltraRare = init["Inventories.card_pack_ultraRare"];
+  const initCardPackUltraRare = init['Inventories.card_pack_ultraRare'];
   //   console.log("처음카드", initCards);
   const newCards = new UltraRareCard();
   const newCardSet = [...initCards, newCards];
@@ -108,7 +108,7 @@ router.post("/open_ultraRare_card_pack", async (req, res) => {
     { card_pack_ultraRare: initCardPackUltraRare - 1 },
     { where: { user_id: id } }
   );
-  res.send("개봉완료");
+  res.send('개봉완료');
 });
 
 module.exports = router;
